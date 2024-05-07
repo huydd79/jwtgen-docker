0248e65d5b060fd46c98e518997715266716021e
Huy Do <86530496+huydd79@users.noreply.github.com>
2024-05-07 21:10:06 +0700

    Update jwtgen-refresh.sh

--- jwtgen-refresh.sh
+++ jwtgen-refresh.sh
@@ -3,5 +3,5 @@
 LOG_FILE=/var/log/jwtgen.log
 
 echo -n "$(date) Refreshing jwt value..." >> $LOG_FILE
-/usr/local/bin/jwtgen.sh /etc/jwtgen/jwt_header.json /etc/jwtgen/jwt_payload.json /etc/ssl/private/keyprv.pem > /var/run/jwtgen/jwt
+/usr/local/bin/jwtgen.sh /etc/jwtgen/jwt_header.json /etc/jwtgen/jwt_payload.json /etc/ssl/private/jwtgen.key > /var/run/jwtgen/jwt
 [[ $? -eq 0 ]] && echo " done" >> $LOG_FILE  || echo " failed" >> $LOG_FILE
